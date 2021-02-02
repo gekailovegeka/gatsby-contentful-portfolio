ccf95705302ad021b58280de8aff91749d9d6292
gekailovegeka <gekailovegeka@gmail.com>
2021-02-02 16:13:58 +0200

    minor changes

--- aws_structure/app.py
+++ aws_structure/app.py
@@ -8,7 +8,7 @@ from aws_cdk import (
     aws_cloudfront as cloudfront,
     aws_dynamodb as dynamodb
 )
-AWS_BUCKET_NAME = 'devops-test-for-alpacked1'
+AWS_BUCKET_NAME = 'devops-test-for-alpacked'
 from aws_structure.aws_structure_stack import AwsStructureStack
 class TestStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
@@ -19,9 +19,17 @@ class TestStack(core.Stack):
         #auto_delete_objects=True,
         access_control=s3.BucketAccessControl.PUBLIC_READ)
         my_bucket.grant_public_access()
+        #Create Cloudfront 
+        cloudfront.
+
         
-        #my_bucket.add_to_resource_policy(iam.PolicyStatement(effect=iam.Effect.ALLOW,actions=["s3:GetObject"],resources=[my_bucket.bucket_arn],principals=[iam.Anyone()]))
+        
             
 app = core.App()
 TestStack(app, "TestStack")
 app.synth()
+
+
+
+
+#my_bucket.add_to_resource_policy(iam.PolicyStatement(effect=iam.Effect.ALLOW,actions=["s3:GetObject"],resources=[my_bucket.bucket_arn],principals=[iam.Anyone()]))
